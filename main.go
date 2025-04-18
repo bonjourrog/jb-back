885e3a2bc8dd5f3cf0882774a9c1b856b608166d
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-04-18 16:54:49 -0700

    feat: implement the serve function in main.go to start server

--- main.go
+++ main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"log"
+	"os"
 
+	"github.com/bonjourrog/jb/routes"
 	"github.com/joho/godotenv"
+)
+
+var (
+	httpRouter routes.Router = routes.NewGinRouter()
 )
 
 func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Println("Error loading env file")
 	}
+
+	httpRouter.Serve(os.Getenv("PORT"))
 }
