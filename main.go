71f5f2470df4cdfbf28d341283da1207e4f47446
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-04-20 17:16:28 -0700

    feat(auth): implement signup endpoint

--- main.go
+++ main.go
@@ -4,18 +4,26 @@ import (
 	"log"
 	"os"
 
+	"github.com/bonjourrog/jb/controller"
+	"github.com/bonjourrog/jb/repository/auth"
 	"github.com/bonjourrog/jb/routes"
+	"github.com/bonjourrog/jb/service"
 	"github.com/joho/godotenv"
 )
 
 var (
-	httpRouter routes.Router = routes.NewGinRouter()
+
+	// Auth
+	authRepo       auth.AuthRepo             = auth.NewAuthRepository()
+	authService    service.AuthService       = service.NewAuthService(authRepo)
+	authController controller.AuthController = controller.NewAuthController(authService)
+	httpRouter     routes.Router             = routes.NewGinRouter()
 )
 
 func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Println("Error loading env file")
 	}
-
+	httpRouter.POST("/api/auth/signup", authController.Signup)
 	httpRouter.Serve(os.Getenv("PORT"))
 }
