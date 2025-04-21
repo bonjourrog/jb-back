a5a95d9cdaa5ac3ac45b33b55af6e2b8cec66c6e
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-04-20 17:15:38 -0700

    feat(auth): add signup controller with request handling, role verification and input validation

---
+++ controller/auth.go
@@ -0,0 +1,73 @@
+package controller
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/bonjourrog/jb/entity"
+	"github.com/bonjourrog/jb/service"
+	"github.com/bonjourrog/jb/util"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/v2/bson"
+)
+
+type AuthController interface {
+	Signup(c *gin.Context)
+}
+
+type authController struct{}
+
+var (
+	_authService service.AuthService
+)
+
+func NewAuthController(authService service.AuthService) AuthController {
+	_authService = authService
+	return &authController{}
+}
+
+// Signup handles user registration by validating input and creating a new user account.
+func (*authController) Signup(c *gin.Context) {
+	var user entity.User
+	if err := json.NewDecoder(c.Request.Body).Decode(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	if user.Name == "" || user.LastName == "" || user.Account.Email == "" || user.Account.Password == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "some required fields are empty",
+		})
+		return
+	}
+	if isRoleValid := util.VerifyRole(user.Role); !isRoleValid {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "invalid role",
+		})
+		return
+	}
+	hashedPassword, err := util.GeneratePassword(user.Account.Password)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	user.ID = bson.NewObjectID()
+	user.Account.Password = hashedPassword
+	user.Account.Banned = false
+	user.CreatedAt = time.Now()
+	user.UpdatedAt = time.Now()
+	_, err = _authService.Signup(user)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "user created",
+	})
+}
