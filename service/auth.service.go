336e3ba32fcd4836f5bf7dd8eafd6c1da478c06b
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-04-23 15:09:46 -0700

    feat(auth): create signin method in auth service

--- service/auth.service.go
+++ service/auth.service.go
@@ -2,14 +2,20 @@ package service
 
 import (
 	"errors"
+	"os"
+	"strings"
+	"time"
 
 	"github.com/bonjourrog/jb/entity"
 	"github.com/bonjourrog/jb/repository/auth"
+	"github.com/bonjourrog/jb/util"
+	"github.com/golang-jwt/jwt/v5"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 type AuthService interface {
 	Signup(user entity.User) (*mongo.InsertOneResult, error)
+	SignIn(credentials entity.Account) (string, error)
 }
 
 type authService struct{}
@@ -33,4 +39,37 @@ func (*authService) Signup(user entity.User) (*mongo.InsertOneResult, error) {
 		return nil, errors.New("email already exists")
 	}
 	return _authRepository.Create(user)
+}
+func (*authService) SignIn(credentials entity.Account) (string, error) {
+	user, err := _authRepository.FindByEmail(credentials.Email)
+	if err != nil {
+		return "", err
+	}
+	if user == nil {
+		return "", errors.New("incorrect email")
+	}
+	if strings.TrimSpace(credentials.Password) == "" || strings.TrimSpace(credentials.Email) == "" {
+		return "", errors.New("some required fields are empty")
+	}
+	if ok := util.ComparePassword([]byte(user.Account.Password), []byte(credentials.Password)); !ok {
+		return "", errors.New("password incorrect")
+	}
+	claims := entity.CustomeClaims{
+		Role:   user.Role,
+		Email:  user.Account.Email,
+		UserID: user.ID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(72 * time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	mySigningKey := os.Getenv("SigningKey")
+	ss, err := token.SignedString([]byte(mySigningKey))
+	if err != nil {
+		return "", err
+	}
+
+	return ss, nil
 }
