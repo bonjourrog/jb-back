9d27bbdef3fb5024397617672366009f37b2aba9
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-04-25 21:10:45 -0700

    feat(service): implement function newJob and validate all job data before passing to repo

---
+++ service/post.service.go
@@ -0,0 +1,45 @@
+package service
+
+import (
+	"errors"
+	"strings"
+	"time"
+
+	"github.com/bonjourrog/jb/entity/job"
+	_job "github.com/bonjourrog/jb/repository/job"
+	"go.mongodb.org/mongo-driver/v2/bson"
+)
+
+type JobService interface {
+	NewJob(job job.Post) error
+}
+type jobService struct{}
+
+var (
+	_jobRepo _job.JobRepository
+)
+
+func NewPostService(jobRepo _job.JobRepository) JobService {
+	_jobRepo = jobRepo
+	return &jobService{}
+}
+func (*jobService) NewJob(job job.Post) error {
+
+	job.Title = strings.TrimSpace(strings.ToLower(job.Title))
+	job.ShortDescription = strings.TrimSpace(job.ShortDescription)
+	job.Description = strings.TrimSpace(job.Description)
+
+	if job.Title == "" || job.ShortDescription == "" || job.Description == "" || job.CompanyID == "" || job.ContractType == "" || job.Industry == "" || job.Schedule == "" {
+		return errors.New("some required fields are empty")
+	}
+
+	job.ID = bson.NewObjectID()
+	job.IsFormalJob = true
+	job.CreatedAt = time.Now()
+	job.UpdatedAt = time.Now()
+
+	if err := _jobRepo.Create(job); err != nil {
+		return err
+	}
+	return nil
+}
