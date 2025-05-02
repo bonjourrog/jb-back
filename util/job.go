9ea9bf19540ffbf8b3e9ec7fd1e5749d8621762f
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-05-02 14:21:35 -0700

    feat(util): create VerifyContractType util function to validate a correct contract type

--- util/job.go
+++ util/job.go
@@ -2,16 +2,33 @@ package util
 
 import "errors"
 
-var validSchedule = map[string]bool{
-	"nocturno":   true,
-	"vespertino": true,
-	"matutino":   true,
-	"rotativo":   true,
-}
+var (
+	validSchedule = map[string]bool{
+		"nocturno":   true,
+		"vespertino": true,
+		"matutino":   true,
+		"rotativo":   true,
+	}
+
+	contractType = map[string]bool{
+		"medio tiempo":    true,
+		"tiempo completo": true,
+		"practicante":     true,
+		"temporal":        true,
+		"proyecto":        true,
+		"freelance":       true,
+	}
+)
 
 func VerifySchedule(schedule string) error {
 	if !validSchedule[schedule] {
 		return errors.New("invalid schedule")
+	}
+	return nil
+}
+func VerifyContractType(contract string) error {
+	if !contractType[contract] {
+		return errors.New("invalid contract")
 	}
 	return nil
 }
