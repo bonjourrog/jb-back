64d97622e862e514221513c245f7c284ce17c422
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-05-02 14:23:34 -0700

    feat(repo): create GetAll function that retrieve all jobs based on some filters

--- repository/job/mongo.repo.go
+++ repository/job/mongo.repo.go
@@ -6,10 +6,13 @@ import (
 
 	"github.com/bonjourrog/jb/db"
 	"github.com/bonjourrog/jb/entity/job"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
 type JobRepository interface {
 	Create(job job.Post) error
+	GetAll(filter bson.M) ([]job.Post, error)
 }
 
 type jobRepository struct{}
@@ -33,4 +36,28 @@ func (*jobRepository) Create(job job.Post) error {
 		return err
 	}
 	return nil
+}
+func (*jobRepository) GetAll(filter bson.M) ([]job.Post, error) {
+	var (
+		_db     = db.NewMongoConnection()
+		results []job.Post
+	)
+
+	client := _db.Connection()
+	defer func() {
+		client.Disconnect(context.TODO())
+	}()
+	coll := client.Database(os.Getenv("DATABASE")).Collection("jobs")
+	findOptions := options.Find()
+	findOptions.SetLimit(12)
+	cursor, err := coll.Find(context.TODO(), filter, findOptions)
+	if err != nil {
+		return results, err
+	}
+	defer cursor.Close(context.TODO())
+	if err = cursor.All(context.TODO(), &results); err != nil {
+		return results, err
+	}
+
+	return results, nil
 }
