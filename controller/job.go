db03b716cf855dbf44f3ef608d3b7d6b4728cb48
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-05-02 14:25:43 -0700

    feat(controller): implement GetJobs handler
    get, validate and set all filters passing by the user in query params

--- controller/job.go
+++ controller/job.go
@@ -6,11 +6,14 @@ import (
 
 	"github.com/bonjourrog/jb/entity/job"
 	"github.com/bonjourrog/jb/service"
+	"github.com/bonjourrog/jb/util"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
 type JobController interface {
 	NewJob(c *gin.Context)
+	GetJobs(c *gin.Context)
 }
 
 type jobController struct{}
@@ -53,4 +56,51 @@ func (*jobController) NewJob(c *gin.Context) {
 		"message": "job created succesfully",
 	})
 
+}
+func (*jobController) GetJobs(c *gin.Context) {
+	var (
+		filter = bson.M{}
+		jobs   []job.Post
+	)
+	query := c.Request.URL.Query()
+	if search := query.Get("search"); search != "" {
+		orFilter := bson.A{
+			bson.M{"title": bson.M{"$regex": search, "$options": "i"}},
+			bson.M{"short_description": bson.M{"$regex": search, "$options": "i"}},
+			bson.M{"description": bson.M{"$regex": search, "$options": "i"}},
+		}
+		filter["$or"] = orFilter
+	}
+	if schedule := query.Get("schedule"); schedule != "" {
+		if err := util.VerifySchedule(schedule); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": err.Error(),
+			})
+			return
+		}
+		filter["schedule"] = schedule
+	}
+	if contract := query.Get("contract"); contract != "" {
+		if err := util.VerifyContractType(contract); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": err.Error(),
+			})
+			return
+		}
+		filter["contract_type"] = contract
+	}
+	if industry := query.Get("industry"); industry != "" {
+		filter["industry"] = industry
+	}
+	jobs, err := _jobService.GetJobs(filter)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "successful request",
+		"data":    jobs,
+	})
 }
