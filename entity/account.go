95f722ea0a020e1483feec9b49704643750a4e20
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-05-24 19:40:14 -0700

    chore(entity): add phone field in Account entity

--- entity/account.go
+++ entity/account.go
@@ -3,5 +3,6 @@ package entity
 type Account struct {
 	Email    string `json:"email" bson:"email"`
 	Password string `json:"password" bson:"password"`
+	Phone    string `json:"phone" bson:"phone"`
 	Banned   bool   `json:"banned" bson:"banned"`
 }
