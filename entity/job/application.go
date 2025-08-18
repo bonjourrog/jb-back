55dca1b1738baae0f9fe38d5754f425d34469520
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-08-17 21:55:21 -0700

    refactor(entity): add a new field into Application entity name AppliedAt

--- entity/job/application.go
+++ entity/job/application.go
@@ -1,11 +1,14 @@
 package job
 
 import (
+	"time"
+
 	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
 type Application struct {
-	ID     bson.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	UserID bson.ObjectID `json:"user_id" bson:"user_id"`
-	JobID  bson.ObjectID `json:"job_id" bson:"job_id"`
+	ID        bson.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	UserID    bson.ObjectID `json:"user_id" bson:"user_id"`
+	JobID     bson.ObjectID `json:"job_id" bson:"job_id"`
+	AppliedAt time.Time     `json:"applied_at" bson:"applied_at"`
 }
