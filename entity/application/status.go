6e6d6fdfe7b0bef16623efdebe3dc1f4f70305a3
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-09-07 17:17:32 -0700

    refactor: type each const value to Status type

--- entity/application/status.go
+++ entity/application/status.go
@@ -3,11 +3,11 @@ package application
 type Status string
 
 const (
-	StatusReceived  = "Received"
-	StatusViewed    = "Viewed"
-	StatusInProcess = "InProcess"
-	StatusRejected  = "Rejected"
-	StatusAccepted  = "Accepted"
-	StatusCancelled = "Cancelled"
-	StatusOnHold    = "OnHold"
+	StatusReceived  Status = "Received"
+	StatusViewed    Status = "Viewed"
+	StatusInProcess Status = "InProcess"
+	StatusRejected  Status = "Rejected"
+	StatusAccepted  Status = "Accepted"
+	StatusCancelled Status = "Cancelled"
+	StatusOnHold    Status = "OnHold"
 )
