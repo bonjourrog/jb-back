488d93f8ba0d70e0ce76b73951c92d5ed7d00a24
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-10-04 17:24:08 -0700

    refactor(repo): change aggregation implementation
    fix total bug: it doest return a correct value when user is logged in

--- repository/job/mongo.repo.go
+++ repository/job/mongo.repo.go
@@ -41,10 +41,10 @@ func (r *jobRepository) GetAll(filter bson.M, page int, ctx context.Context) ([]
 		lookupStage bson.D
 	)
 	coll := r.client.Database(os.Getenv("DATABASE")).Collection("jobs")
-	total, err := coll.CountDocuments(ctx, filter)
-	if err != nil {
-		return nil, 0, err
-	}
+	// total, err := coll.CountDocuments(ctx, filter)
+	// if err != nil {
+	// 	return nil, 0, err
+	// }
 	var userId = filter["user_id"]
 	if userId != bson.NilObjectID {
 		lookupStage = bson.D{{
@@ -69,54 +69,103 @@ func (r *jobRepository) GetAll(filter bson.M, page int, ctx context.Context) ([]
 	}
 	delete(filter, "user_id")
 
-	pipeline := mongo.Pipeline{
-		lookupStage,
-		{
-			{"$match", bson.M{"userApplications": bson.M{"$size": 0}}},
-		},
-		// Filtrado
-		{{"$match", filter}},
+	basePipeline := mongo.Pipeline{}
+	if len(lookupStage) > 0 {
+		basePipeline = append(basePipeline, lookupStage)
+		basePipeline = append(basePipeline, bson.D{{"$match", bson.M{"userApplications": bson.M{"$size": 0}}}})
+	}
 
-		// Paginación
-		{{"$skip", int64(skip)}},
-		{{"$limit", int64(limit)}},
+	if len(filter) > 0 {
+		basePipeline = append(basePipeline, bson.D{{"$match", filter}})
+	}
+
+	countPipeline := append(basePipeline, bson.D{{"$count", "total"}})
+
+	countCursor, err := coll.Aggregate(ctx, countPipeline)
+	if err != nil {
+		return nil, 0, err
+	}
+	var countResult []bson.M
+	if err = countCursor.All(ctx, &countResult); err != nil {
+		return nil, 0, err
+	}
+
+	var totalCount int64 = 0
+	if len(countResult) > 0 {
+		totalCount = int64(countResult[0]["total"].(int32))
+	}
+
+	// pipeline := mongo.Pipeline{
+	// 	lookupStage,
+	// 	{
+	// 		{"$match", bson.M{"userApplications": bson.M{"$size": 0}}},
+	// 	},
+	// 	// Filtrado
+	// 	{{"$match", filter}},
+
+	// 	// Paginación
+	// 	{{"$skip", int64(skip)}},
+	// 	{{"$limit", int64(limit)}},
+
+	// 	// Join con companies
+	// 	{{"$lookup", bson.M{
+	// 		"from":         "users",
+	// 		"localField":   "company_id",
+	// 		"foreignField": "_id",
+	// 		"as":           "company",
+	// 	}}},
+	// 	{{"$unwind", bson.M{
+	// 		"path":                       "$company",
+	// 		"preserveNullAndEmptyArrays": true,
+	// 	}}},
+	// 	{{"$addFields", bson.M{
+	// 		"company_name": "$company.company.name",
+	// 		"company_logo": "$company.company.logo",
+	// 	}}},
+	// }
+
+	// cursor, err := coll.Aggregate(ctx, basePipeline)
+
+	// if err != nil {
+	// 	return results, 0, err
+	// }
+	// defer cursor.Close(ctx)
+	// if err = cursor.All(ctx, &results); err != nil {
+	// 	return results, 0, err
+	// }
+
+	dataPipeline := append(basePipeline,
+		bson.D{{"$skip", int64(skip)}},
+		bson.D{{"$limit", int64(limit)}},
 
 		// Join con companies
-		{{"$lookup", bson.M{
+		bson.D{{"$lookup", bson.M{
 			"from":         "users",
 			"localField":   "company_id",
 			"foreignField": "_id",
 			"as":           "company",
 		}}},
-		{{"$unwind", bson.M{
+		bson.D{{"$unwind", bson.M{
 			"path":                       "$company",
 			"preserveNullAndEmptyArrays": true,
 		}}},
-		{{"$addFields", bson.M{
+		bson.D{{"$addFields", bson.M{
 			"company_name": "$company.company.name",
 			"company_logo": "$company.company.logo",
 		}}},
-	}
+	)
 
-	// findOptions := options.Find()
-	// findOptions.SetSkip(int64(skip)).SetLimit(int64(limit))
-	// cursor, err := coll.Find(context.TODO(), filter, findOptions)
-	cursor, err := coll.Aggregate(ctx, pipeline)
-	// for cursor.Next(context.TODO()) {
-	// 	var raw bson.M
-	// 	cursor.Decode(&raw)
-	// 	fmt.Printf("%+v\n", raw)
-	// 	break
-	// }
+	cursor, err := coll.Aggregate(ctx, dataPipeline)
 	if err != nil {
 		return results, 0, err
 	}
 	defer cursor.Close(ctx)
+
 	if err = cursor.All(ctx, &results); err != nil {
 		return results, 0, err
 	}
 
-	return results, total, nil
+	return results, totalCount, nil
 }
 
 func (r *jobRepository) Update(job job.Post, ctx context.Context) error {
