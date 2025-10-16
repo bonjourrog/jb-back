e25c1ec9753893b81965cf20d1f7a9c8d7f47c56
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-10-16 08:50:44 -0700

    feat(util): create slugify function

--- util/job.go
+++ util/job.go
@@ -1,6 +1,11 @@
 package util
 
-import "errors"
+import (
+	"errors"
+	"regexp"
+	"strings"
+	"unicode"
+)
 
 var (
 	validSchedule = map[string]bool{
@@ -31,4 +36,52 @@ func VerifyContractType(contract string) error {
 		return errors.New("invalid contract")
 	}
 	return nil
+}
+
+var (
+	nonAlnumDash = regexp.MustCompile(`[^a-z0-9-]`)
+	multiDash    = regexp.MustCompile(`-+`)
+)
+
+func Slugify(s string) string {
+	s = strings.ToLower(s)
+
+	var b strings.Builder
+	b.Grow(len(s))
+	for _, r := range s {
+		switch r {
+		case 'á', 'à', 'ä', 'â', 'ã', 'å':
+			r = 'a'
+		case 'é', 'è', 'ë', 'ê':
+			r = 'e'
+		case 'í', 'ì', 'ï', 'î':
+			r = 'i'
+		case 'ó', 'ò', 'ö', 'ô', 'õ':
+			r = 'o'
+		case 'ú', 'ù', 'ü', 'û':
+			r = 'u'
+		case 'ñ':
+			r = 'n'
+		case 'ç':
+			r = 'c'
+		}
+		// Reemplaza espacio y separadores Unicode por guion.
+		if unicode.IsSpace(r) || unicode.Is(unicode.Zs, r) {
+			b.WriteRune('-')
+			continue
+		}
+		b.WriteRune(r)
+	}
+	s = b.String()
+
+	// 3) Sustituye cualquier carácter no permitido por nada.
+	s = nonAlnumDash.ReplaceAllString(s, "")
+
+	// 4) Colapsa guiones múltiples en uno solo.
+	s = multiDash.ReplaceAllString(s, "-")
+
+	// 5) Recorta guiones al inicio y final.
+	s = strings.Trim(s, "-")
+
+	return s
 }
