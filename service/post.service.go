fd49a396b523f19d3245cfa9f04e6b1baec742a3
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-10-16 08:53:13 -0700

    feat(serive):implemdent getBySlug function that retrieve a job by its slug value

--- service/post.service.go
+++ service/post.service.go
@@ -6,9 +6,12 @@ import (
 	"strings"
 	"time"
 
+	"github.com/bonjourrog/jb/entity"
 	"github.com/bonjourrog/jb/entity/job"
 	"github.com/bonjourrog/jb/repository/application"
 	_job "github.com/bonjourrog/jb/repository/job"
+	"github.com/bonjourrog/jb/repository/user"
+	"github.com/bonjourrog/jb/util"
 	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
@@ -17,18 +20,21 @@ type JobService interface {
 	GetJobs(filter bson.M, page int, ctx context.Context) ([]job.PostWithCompany, int64, error)
 	UpdateJob(job job.Post, ctx context.Context) error
 	DeleteJob(job_id bson.ObjectID, user_id bson.ObjectID, ctx context.Context) error
+	GetBySlug(company_name, slug string, ctx context.Context) (*job.PostWithCompany, error)
 }
 type jobService struct {
-	jobRepo _job.JobRepository
-	appRepo application.ApplicationRepository
+	jobRepo  _job.JobRepository
+	appRepo  application.ApplicationRepository
+	userRepo user.UserRepo
 }
 
 var ()
 
-func NewPostService(jobRepo _job.JobRepository, appRepo application.ApplicationRepository) JobService {
+func NewPostService(jobRepo _job.JobRepository, appRepo application.ApplicationRepository, userRepo user.UserRepo) JobService {
 	return &jobService{
-		jobRepo: jobRepo,
-		appRepo: appRepo,
+		jobRepo:  jobRepo,
+		appRepo:  appRepo,
+		userRepo: userRepo,
 	}
 }
 func (j *jobService) NewJob(job job.Post, ctx context.Context) (*job.Post, error) {
@@ -46,6 +52,7 @@ func (j *jobService) NewJob(job job.Post, ctx context.Context) (*job.Post, error
 	job.Published = true
 	job.CreatedAt = time.Now()
 	job.UpdatedAt = time.Now()
+	job.Slug = bson.NewObjectID().Hex() + "-" + util.Slugify(job.Title)
 
 	insertedID, err := j.jobRepo.Create(job, ctx)
 	if err != nil {
@@ -107,4 +114,29 @@ func (j *jobService) DeleteJob(job_id bson.ObjectID, company_id bson.ObjectID, c
 	}
 
 	return j.jobRepo.Delete(job_id, company_id, ctx)
+}
+func (j *jobService) GetBySlug(company_name, slug string, ctx context.Context) (*job.PostWithCompany, error) {
+	if company_name == "" || slug == "" {
+		return nil, entity.ErrMissingField
+	}
+	_job, err := j.jobRepo.FindByField("slug", slug, ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	user, err := j.userRepo.FindByField("_id", _job.CompanyID, ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	if strings.ReplaceAll(strings.ToLower(company_name), "-", " ") != strings.ToLower(user.Company.Name) {
+		return nil, entity.ErrUnauthorized
+	}
+	var jjob job.PostWithCompany = job.PostWithCompany{
+		Post:         *_job,
+		CompanyName:  user.Company.Name,
+		CompanyLogo:  user.Company.Logo,
+		CompanyPhone: user.Account.Phone,
+	}
+	return &jjob, nil
 }
